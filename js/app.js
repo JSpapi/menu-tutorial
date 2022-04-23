8a92d7014a846dc3b6c68a0ae80d32144bde808f
axror <axror.kurban@gmail.com>
2022-04-23 20:14:49 +0500

    my-menu

---
+++ js/app.js
@@ -0,0 +1,162 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 9,
+    title: "stake",
+    category: "dinner",
+    price: 30.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
+
+// ! DOWNLOAD MENU ITEMS
+window.addEventListener('DOMContentLoaded', ()=>{
+  showMenuItems(menu);
+
+  showMenuBtns();
+})
+
+// ? SHOW MENU ITEMS FUNCTION
+
+const showMenuItems = menuItems =>{
+  const showMenu = menuItems.map(item => {
+    return `
+      <article class="menu-item">
+        <img class="photo" src=${item.img} alt=${item.title}>
+
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">$${item.price}</h4>
+          </header>
+
+          <p class="item-text">
+            ${item.desc}
+          </p>
+        </div>
+      </article>
+    `;
+  }).join('');
+
+  sectionCenter.innerHTML = showMenu;
+}
+
+// ? SHOW CATEGORY FUNCTION
+
+const showMenuBtns = ()=> {
+  // todo GET UNIQUE CATEGORY 
+  const categories = menu.reduce((values, item) => {
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    };
+
+    return values;
+  }, ['all']);
+
+  // todo SHOW BTN
+  const showBtns = categories.map(category => {
+    return `
+      <button class="filter-btn" data-id="${category}" type="button">${category}</button> 
+    `;
+  }).join('');
+
+  btnContainer.innerHTML = showBtns;
+
+  // todo SHOW MENU DEPENDING ON CATEGORY
+
+  const filterBtns = document.querySelectorAll('.filter-btn');
+
+  filterBtns.forEach(btn => {
+    btn.addEventListener('click', e =>{
+      const category = e.target.dataset.id;
+
+      const filteredMenu = menu.filter(menuItem => {
+        if(menuItem.category === category){
+          return menuItem;
+        }
+      });
+
+      if(category === 'all'){
+        showMenuItems(menu);
+      }else{
+        showMenuItems(filteredMenu);
+      }''
+    });
+  });
+}
